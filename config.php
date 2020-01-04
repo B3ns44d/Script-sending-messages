d65f809f4533aa7cd9c44d7151043b0c05afc98b
Abdessamad Bensaad <abdsamad23bnsaad@gmail.com>
2020-01-04 20:47:29 +0100

    add all fils

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $empfaenger = "Your@E-Mail.com";  // Please enter your email address here. (between the inverted comma)
+    $ihrname = "Your Name"; // Please enter your name here. (between the inverted comma) It will appear as consigner in the thank mail.
+    
+    
+    $danke = "thankyou.php";  // URL to thank you page.
+    // All of these things were programmed by Abdessamad Bensaad from scratch
+?> 
---
+++ thankyou.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="de-DE">
+	<head>
+		<meta charset="utf-8">
+		<meta name="language" content="de"/>
+		<meta name="description" content="pro16.herokuapp.com"/>
+		<meta name="revisit" content="After 7 days"/>
+		<meta name="robots" content="INDEX,FOLLOW"/>
+		<title>pro16.herokuapp.com</title>
+	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+<style type="text/css">
+body{font-family:Verdana,Arial,Helvetica,sans-serif;font-size:14px;color:#000000;background-color: #FFFFFF;}
+a:link, a:visited, a:active{color:#000000;text-decoration:none;}
+a:hover{text-decoration: underline;}
+</style>
+
+	</head>
+
+
+
+<body>
+
+<div>
+
+ 
+    
+	<p>Thank you very much. Your message has been transfered succesfully.</p>		
+ <p>
+ <a href="https://www.pro16.herokuapp.com" onclick="window.open(this.href); return false;" title="pro16.herokuapp.com" style="font-size:12px; "><!-- It´s not allowed to remove the copyright notice! -->&copy; by pro16.herokuapp.com</a></p>
+
+</div>
+
+</body>
+</html>
